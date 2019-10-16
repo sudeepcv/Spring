bdeb1e7692e3ce128ea014000c49b6551d9c51e3
sudeepcv <sudeep.cv@piserve.com>
2019-10-16 10:49:25 +0530

    init

---
+++ src/main/java/com/basics/in/java/blogspot/Spring/Application.java
@@ -0,0 +1,13 @@
+package com.basics.in.java.blogspot.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
