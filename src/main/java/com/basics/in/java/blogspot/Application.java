bbf587cc86fc22a1154f4ce3d4c3bbca0c09f122
Sudeep-cv <basicsinjava@gmail.com>
2019-10-21 09:49:23 +0000

    applicationcontext

--- src/main/java/com/basics/in/java/blogspot/Application.java
+++ src/main/java/com/basics/in/java/blogspot/Application.java
@@ -1,29 +1,20 @@
 package com.basics.in.java.blogspot;
 
-
-import org.hibernate.validator.internal.util.logging.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import ch.qos.logback.classic.Logger;
 
 @SpringBootApplication
 public class Application {
-//    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-	public static void main(String[] args) {
-         ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+    public static void main(String[] args) {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
 
-         String[] BeanNames = applicationContext.getBeanFactory().getBeanDefinitionNames();
+        String[] BeanNames = applicationContext.getBeanFactory().getBeanDefinitionNames();
 
-         for(String beanName:BeanNames){
-             if(beanName.equals("laptop"))
-             System.out.println("\n Bean aname:"+beanName);
-         }
+        for (String beanName : BeanNames) {
+            System.out.println("\n Bean aname:" + beanName);
+        }
 
-
-
-}
+    }
 }
